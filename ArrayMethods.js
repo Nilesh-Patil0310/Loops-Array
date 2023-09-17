44408e66faf308ca66ab28e834b98adb77ea52c5
djangowithnilesh <djangowithnilesh@gmail.com>
2023-09-17 21:26:52 +0530

    Array Method study

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -1,10 +1,45 @@
 /** Arrays js are Dynamic */
 
-const cities = [
-    'Munmbai', 
-    'Delhi', 
-    'Kolkata', 
-    'Chennai', 
-     'Pune'
-];
+// const cities = [
+//     'Munmbai', 
+//     'Delhi', 
+//     'calcutta', 
+//     'Chennai', 
+//      'Pune'
+// ];
 
+// console.log(cities.length)
+
+// cities[2] = 'Kolkata'
+
+// console.log(cities)
+
+/** Adding an element  */
+
+// push()
+
+const teams = ['CSK','RCB','MI','LSG'];
+console.log(teams); 
+
+const pushArray = teams.push('GT');
+
+console.log(pushArray);
+
+// unshift()
+const unshiftArray = teams.unshift('KKR');
+console.log(unshiftArray)
+
+/** Removing an elents */
+
+// pop()
+const popArray = teams.pop();
+console.log(popArray);
+// shift()
+const shiftArray = teams.shift();
+console.log(shiftArray);
+
+/** Check the element is present or not */
+
+// indexof()
+
+// includes()
