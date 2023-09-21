08ad7b19006bc12cfa75ae8c5fe05a88b8618bc8
djangowithnilesh <djangowithnilesh@gmail.com>
2023-09-21 14:04:54 +0530

    add Rest_vs_spread file in pratice

--- Break_continue.js
+++ Break_continue.js
@@ -1,17 +1,17 @@
-const input = [1,2,3,4,5,6,7,8,10,11,12,13];
+// const input = [1,2,3,4,5,6,7,8,10,11,12,13];
 
-const evenNumber = [];
+// const evenNumber = [];
 
-for (let i of input) {
-    if (i % 2 !== 0) {
-        continue;
-    }
-    evenNumber.push(i);
+// for (let i of input) {
+//     if (i % 2 !== 0) {
+//         continue;
+//     }
+//     evenNumber.push(i);
 
-    if (evenNumber.length === 5){
-        break;
-    }
-    console.log(evenNumber);
-}
+//     if (evenNumber.length === 5){
+//         break;
+//     }
+//     console.log(evenNumber);
+// }
 
-console.log(evenNumber);
+// console.log(evenNumber);
---
+++ Rest_vs_spread_operator.js
@@ -0,0 +1,36 @@
+const aprilbatch = ["kiran", "dada", "dhanu"];
+const maybatch = ["nilu", "ganu"];
+const junebatch = ["manu"];
+const jullybatch = [];
+
+// function addstudent(batch, student) {
+//     batch.push(student);
+//     console.log(batch);
+// }
+
+// addstudent(junebatch, 'dipali');
+// addstudent(maybatch, 'bhushan');
+
+// function using Rest.. operator
+
+function addstudent(batch, ...students) {
+  for (let i of students) {
+    batch.push(i);
+  }
+  console.log(batch);
+}
+
+addstudent(junebatch, "dipali", "nilesh", "patil");
+addstudent(jullybatch, "banti", "nana", "mahesh", "vishal");
+
+// spread operator
+
+// without spread operator
+
+const newbatch = [...aprilbatch];
+
+aprilbatch.push('pravin')
+console.log(newbatch);
+console.log(aprilbatch);
+
+// with spread operator
