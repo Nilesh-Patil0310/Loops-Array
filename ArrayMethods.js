b42a4a2d03a85cf2b3ac9d0e347a86db276edb04
djangowithnilesh <djangowithnilesh@gmail.com>
2023-09-21 13:05:23 +0530

    add loop_over array file

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -1,45 +1,75 @@
-/** Arrays js are Dynamic */
+// /** Arrays js are Dynamic */
 
-// const cities = [
-//     'Munmbai', 
-//     'Delhi', 
-//     'calcutta', 
-//     'Chennai', 
-//      'Pune'
-// ];
+// // const cities = [
+// //     'Munmbai',
+// //     'Delhi',
+// //     'calcutta',
+// //     'Chennai',
+// //      'Pune'
+// // ];
 
-// console.log(cities.length)
+// // console.log(cities.length)
 
-// cities[2] = 'Kolkata'
+// // cities[2] = 'Kolkata'
 
-// console.log(cities)
+// // console.log(cities)
 
-/** Adding an element  */
+// /** Adding an element  */
 
-// push()
+// // push()
 
-const teams = ['CSK','RCB','MI','LSG'];
-console.log(teams); 
+// const teams = ["CSK", "RCB", "MI", "LSG"];
+// console.log(teams);
 
-const pushArray = teams.push('GT');
+// const pushArray = teams.push("GT");
 
-console.log(pushArray);
+// console.log(pushArray);
 
-// unshift()
-const unshiftArray = teams.unshift('KKR');
-console.log(unshiftArray)
+// // unshift()
+// const unshiftArray = teams.unshift("KKR");
+// console.log(unshiftArray);
 
-/** Removing an elents */
+// /** Removing an elents */
 
-// pop()
-const popArray = teams.pop();
-console.log(popArray);
-// shift()
-const shiftArray = teams.shift();
-console.log(shiftArray);
+// // pop()
+// const popArray = teams.pop();
+// console.log(popArray);
+// // shift()
+// const shiftArray = teams.shift();
+// console.log(shiftArray);
 
-/** Check the element is present or not */
+// /** Check the element is present or not */
 
-// indexof()
+// // indexof()
+// const RCBindex = teams.indexOf("RCB");
+// console.log(RCBindex);
+// const GTindex = teams.indexOf("GT");
+// console.log(GTindex);
 
-// includes()
+// // includes()
+// const rcbIncludes = teams.includes("RCB");
+// console.log(rcbIncludes);
+
+// // slice() and splice()
+
+// console.log(teams);
+// console.log(teams.slice(2));
+// console.log(teams.slice(-1));
+// console.log(teams.slice(-2));
+// console.log(teams.slice(-3));
+// console.log(teams);
+
+// // splice()
+
+// teams.splice(2, 0, "GT");
+// console.log(teams);
+
+// teams.splice(2, 1, "HRB", "RR");
+// console.log(teams);
+
+// // concat()
+
+// const teams1 = ["IND", "AUS", "SL"];
+
+// const mergeTeams = teams.concat(teams1);
+// console.log(mergeTeams);
---
+++ Loop_over_array.js
@@ -0,0 +1,24 @@
+// const students = [
+//     "dada", 
+//     "patil", 
+//     "dhanu", 
+//     "kiran", 
+//     "nana", 
+//     "ganu"
+// ];
+
+// // for (let i = 0; i < students.length; i++){
+// //     console.log(`Roll No ${i+1}: ${students[i]}`);
+// // }
+
+// // for in 
+
+// for (let i in students){
+//     console.log(`Roll No ${Number(i) + 1}: ${students[i]}`);
+// }
+
+// // for of
+
+// for (let i of students){
+//     console.log(i);
+// }
